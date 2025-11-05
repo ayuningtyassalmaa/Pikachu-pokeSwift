4dacdeeaa2df7a7267724b7802ad89f5b87ef6a8
Salma Ayuningtyas <ayuningtyas.ayusalma@gmail.com>
2025-11-05 21:32:58 +0700

    changes: - add API pokemon detail - removed unsused codes - add keys in info.plist

---
+++ Pikachu-pokeSwift/Model/PokemonDetailData.swift
@@ -0,0 +1,8 @@
+//
+//  PokemonDetailData.swift
+//  Pikachu-pokeSwift
+//
+//  Created by Salma Ayuningtyas on 11/5/25.
+//
+
+import Foundation
---
+++ Pikachu-pokeSwift/View-Model/PokemonDetailsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  PokemonDetailsViewModel.swift
+//  Pikachu-pokeSwift
+//
+//  Created by Salma Ayuningtyas on 11/5/25.
+//
+
+import Foundation
